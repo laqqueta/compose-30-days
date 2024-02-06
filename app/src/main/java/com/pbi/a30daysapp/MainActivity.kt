6addedf39e7f5bb52d6ae0d512b797f57e93d6e7
je <envyxbot@gmail.com>
2024-02-06 13:09:36 +0800

    init files

---
+++ app/src/main/java/com/pbi/a30daysapp/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.pbi.a30daysapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.pbi.a30daysapp.ui.theme._30DaysAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            _30DaysAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(device = "spec:width=360dp,height=800dp", showSystemUi = true, showBackground = true)
+@Composable
+fun GreetingPreview() {
+    _30DaysAppTheme {
+
+    }
+}
