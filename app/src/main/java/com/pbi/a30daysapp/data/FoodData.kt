8874b0b7e82b93b18bb1abb3742f207a1c07f6d3
je <envyxbot@gmail.com>
2024-02-07 16:55:57 +0800

    create data for Food

---
+++ app/src/main/java/com/pbi/a30daysapp/data/FoodData.kt
@@ -0,0 +1,160 @@
+package com.pbi.a30daysapp.data
+
+import androidx.collection.ObjectList
+import androidx.collection.objectListOf
+import com.pbi.a30daysapp.R
+import com.pbi.a30daysapp.model.Food
+
+object FoodData {
+    val foods: ObjectList<Food> = objectListOf(
+        Food(
+            foodTitle = R.string.food_title_01,
+            foodDesc = R.string.food_desc_01,
+            foodImage = R.drawable.image_01
+        ),
+        Food(
+            foodTitle = R.string.food_title_02,
+            foodDesc = R.string.food_desc_02,
+            foodImage = R.drawable.image_02
+        ),
+        Food(
+            foodTitle = R.string.food_title_03,
+            foodDesc = R.string.food_desc_03,
+            foodImage = R.drawable.image_03
+        ),
+        Food(
+            foodTitle = R.string.food_title_04,
+            foodDesc = R.string.food_desc_04,
+            foodImage = R.drawable.image_04
+        ),
+        Food(
+            foodTitle = R.string.food_title_05,
+            foodDesc = R.string.food_desc_05,
+            foodImage = R.drawable.image_05
+        ),
+        Food(
+            foodTitle = R.string.food_title_06,
+            foodDesc = R.string.food_desc_06,
+            foodImage = R.drawable.image_06
+        ),
+        Food(
+            foodTitle = R.string.food_title_07,
+            foodDesc = R.string.food_desc_07,
+            foodImage = R.drawable.image_07
+        ),
+        Food(
+            foodTitle = R.string.food_title_08,
+            foodDesc = R.string.food_desc_08,
+            foodImage = R.drawable.image_08
+        ),
+        Food(
+            foodTitle = R.string.food_title_09,
+            foodDesc = R.string.food_desc_09,
+            foodImage = R.drawable.image_09
+        ),
+        Food(
+            foodTitle = R.string.food_title_10,
+            foodDesc = R.string.food_desc_10,
+            foodImage = R.drawable.image_10
+        ),
+        Food(
+            foodTitle = R.string.food_title_11,
+            foodDesc = R.string.food_desc_11,
+            foodImage = R.drawable.image_11
+        ),
+        Food(
+            foodTitle = R.string.food_title_12,
+            foodDesc = R.string.food_desc_12,
+            foodImage = R.drawable.image_12
+        ),Food(
+            foodTitle = R.string.food_title_13,
+            foodDesc = R.string.food_desc_13,
+            foodImage = R.drawable.image_13
+        ),
+        Food(
+            foodTitle = R.string.food_title_14,
+            foodDesc = R.string.food_desc_14,
+            foodImage = R.drawable.image_14
+        ),
+        Food(
+            foodTitle = R.string.food_title_15,
+            foodDesc = R.string.food_desc_15,
+            foodImage = R.drawable.image_15
+        ),
+        Food(
+            foodTitle = R.string.food_title_16,
+            foodDesc = R.string.food_desc_16,
+            foodImage = R.drawable.image_16
+        ),
+        Food(
+            foodTitle = R.string.food_title_17,
+            foodDesc = R.string.food_desc_17,
+            foodImage = R.drawable.image_17
+        ),
+        Food(
+            foodTitle = R.string.food_title_18,
+            foodDesc = R.string.food_desc_18,
+            foodImage = R.drawable.image_18
+        ),
+        Food(
+            foodTitle = R.string.food_title_19,
+            foodDesc = R.string.food_desc_19,
+            foodImage = R.drawable.image_19
+        ),
+        Food(
+            foodTitle = R.string.food_title_20,
+            foodDesc = R.string.food_desc_20,
+            foodImage = R.drawable.image_20
+        ),
+        Food(
+            foodTitle = R.string.food_title_21,
+            foodDesc = R.string.food_desc_21,
+            foodImage = R.drawable.image_21
+        ),
+        Food(
+            foodTitle = R.string.food_title_22,
+            foodDesc = R.string.food_desc_22,
+            foodImage = R.drawable.image_22
+        ),
+        Food(
+            foodTitle = R.string.food_title_23,
+            foodDesc = R.string.food_desc_23,
+            foodImage = R.drawable.image_23
+        ),
+        Food(
+            foodTitle = R.string.food_title_24,
+            foodDesc = R.string.food_desc_24,
+            foodImage = R.drawable.image_24
+        ),
+        Food(
+            foodTitle = R.string.food_title_25,
+            foodDesc = R.string.food_desc_25,
+            foodImage = R.drawable.image_25
+        ),
+        Food(
+            foodTitle = R.string.food_title_26,
+            foodDesc = R.string.food_desc_26,
+            foodImage = R.drawable.image_26
+        ),
+        Food(
+            foodTitle = R.string.food_title_27,
+            foodDesc = R.string.food_desc_27,
+            foodImage = R.drawable.image_27
+        ),
+        Food(
+            foodTitle = R.string.food_title_28,
+            foodDesc = R.string.food_desc_28,
+            foodImage = R.drawable.image_28
+        ),
+        Food(
+            foodTitle = R.string.food_title_29,
+            foodDesc = R.string.food_desc_29,
+            foodImage = R.drawable.image_29
+        ),
+        Food(
+            foodTitle = R.string.food_title_30,
+            foodDesc = R.string.food_desc_30,
+            foodImage = R.drawable.image_30
+        )
+    )
+}
