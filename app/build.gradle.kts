0c5a3af353d58a4c18626a233bddea72d74219f1
je <envyxbot@gmail.com>
2024-02-07 16:53:24 +0800

    enable minify and shrink resource

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,9 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -50,7 +52,7 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.collection:collection-ktx:1.4.0")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
